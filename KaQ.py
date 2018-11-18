661f422d2c05ca9e41cada22a1a5fbd28bbf5155
Max-Mills <max33134@gmail.com>
2018-11-18 01:50:49 -0500

    more minor changes

--- KaQ.py
+++ KaQ.py
@@ -12,7 +12,8 @@ pile = []
 players = 0
 
 class Deck:
-    #### Builds the deck using the data from json and the SUIT const ###
+
+    #### Builds the deck using the data from the json ###
     def build(self):
         ### Takes the data from the json and puts it into the data variable ###
         with open('Cards.json', 'r') as j:
@@ -43,16 +44,19 @@ class Deck:
         return hands
 
 class Hand:
+
     ### Orders the cards ###
     def order(self, phand):
         phand = sorted(phand, key=itemgetter(0))
         return phand
+
     ### For all cards in your hand print individual card ###
     def print(self, phand):
         print("Here is your hand: ", end= " ")
         for x in phand:
             print ("%s%s" % (x[0],x[1]), end=" " )
         print()
+
     ### Count how many pairs of cards a hand has ### 
     def values(self, phand):
         checklist = []
@@ -61,6 +65,7 @@ class Hand:
         return Counter(checklist)
 
 class Game:
+
     ### Ask the player how many players there are to split the deck evenly ###
     def intro(self):
         print ("♥ Welcome to my card game ♦")
@@ -87,7 +92,7 @@ class Game:
                     return player
             player += 1
 
-    ### asks the player to select a card to put down, and checks if there are no errors ###
+    ### Asks the player to select a card to put down, and checks if there are no errors ###
     def selectcard(self, valuelist, toppile):
         while True:
             putdown = str(test.checkiscard("Which card do you want to put down (put pass to pass) : "))
@@ -105,19 +110,17 @@ class Game:
             ### Checks to see if what you put down is a card you have. If not, informs you ###
             elif putdown not in valuelist:
                 print ("You do not have that card \n")
-            ### If pile is empty and you can play doubles, asks if you'd like to ###
-            elif hmanydown == 2 and toppile == []:
-                num = test.checkiscard("you have doubles of that card, whould you like to play 1 or 2 of it? : ")
-            ### If pile is empty and you have multiples of that card, asks how many you'd like to put down ###
-            elif hmanydown > 2 and toppile == []:
+            ### If pile is empty and you have multiples of that card, asks how many you would like to put down ###
+            elif hmanydown >= 2 and toppile == []:
                 r = list(range(1, hmanydown))
                 formatedr = str(r)[1:-1] 
                 num = test.checkiscard("you have multiples of that card, whould you like to play %s or %s? : " % (formatedr,hmanydown))
             ### If you need to play multiples, it sets that up ###
             elif hmanytop <= hmanydown and toppilenum < putdownnum:
                 num = hmanytop
-            ### Don't have enough of that card to play ###
+            ### You do not have enough of that card to play ###
             elif hmanytop > hmanydown:
+                print("Not enough of that card")
                 num = 0
             ### You have enough to play but the card is too low to play ###
             elif hmanytop <= hmanydown and toppilenum > putdownnum:
@@ -131,11 +134,10 @@ class Game:
             if num <= hmanydown and num > 0:
                 return (num, putdown)
             ### Catch it if putdown is more than 4 ###
-            elif putdownnum > 4:
-                pass
             else:
                 print("You don't have that many of that card")
 
+    ### Remove the card you played from your hand and add it to the pile ###
     def playcard(self, phand, howmany, putdown, pile):
         while howmany != 0:
             for x in phand:
@@ -146,25 +148,28 @@ class Game:
             howmany -= 1
         return phand, pile
 
+    ### Checks to see if there are multiples of the card on top and informs you have how many ###
     def topofpile(self, pile):
         topcard = pile[-1][0]
         if len(pile) > 1 and topcard == pile[-2][0]:
             if len(pile) > 2 and topcard == pile[-3][0]:
                 if len(pile) > 3 and topcard == pile[-4][0]:
-                    print ("Beat Quadruple %s" % (topcard))
+                    print ("Beat Quadruple %s's" % (topcard))
                     toppile = [4,topcard]
                     return toppile
-                print ("Beat Triple %s" % (topcard))
+                print ("Beat Triple %s's" % (topcard))
                 toppile = [3,topcard]
                 return toppile
-            print ("Beat Double %s" % (topcard))
+            print ("Beat Double %s's" % (topcard))
             toppile = [2,topcard]
             return toppile
-        print ("Beat Single %s" % (topcard))
+        print ("Beat a Single %s" % (topcard))
         toppile = [1,topcard]
         return toppile
 
 class test:
+
+    ### Gives a number for the royal cards ###
     def royals(test):
         num = 0
         try:
@@ -182,6 +187,7 @@ class test:
                 pass
         return num
 
+    ### Checks to see if a number or royality was given, if not repeat the question ###
     def checkiscard(question):
         while True:
             tester = input(question)
